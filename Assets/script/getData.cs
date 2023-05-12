a44b201a13a5246bf897401e9a91a6c5976451c0
MelaineF <melaine.favennec@gmail.com>
2023-05-12 09:39:29 +0200

    on peut ajouter des personnes à la bdd

--- Assets/script/getData.cs
+++ Assets/script/getData.cs
@@ -18,6 +18,7 @@
         PanelOpener panelOpener;
         EventSystem eventSystem;
         public GameObject panelControl;
+        public GameObject menuControl;
         ControlCurseur controlCurseur;
         gameSet gameSet;
         
@@ -66,14 +67,24 @@
            
             if(namePlayer.text != ""){
 
-                dataBase.FindPlayer(namePlayer.text, isPlayer2);
 
+                if (isPlayer2 && playerNames[0] == namePlayer.text)
+                {
+                    sameName.enabled = true;
+                }
+                else
+                {
+                dataBase.FindPlayer(namePlayer.text, isPlayer2);
                 panelOpener.OpenPanel();
                 controlCurseur.panelOpen = true;
+                eventSystem.SetSelectedGameObject(panelControl);
+            }
+
+                
 
 
                 //eventSystem.firstSelectedGameObject = panelControl;
-                eventSystem.SetSelectedGameObject(panelControl);
+                
               
                 /*if(!isPlayer2){
                     playerNames[0] = namePlayer.text;
@@ -102,6 +113,7 @@
         }
          public void ValiderPanel(){
             controlCurseur.panelOpen = false;
+            eventSystem.SetSelectedGameObject(menuControl);
            panelOpener.ClosePanel();
 
             if(!isPlayer2){
@@ -109,6 +121,24 @@
             } else {
                 dataBase.UpdatePlayer(gameSet.idPlayer2, controlCurseur.cursorSpeedPlayer.GetComponent<Slider>().value, controlCurseur.CursorRotationSpeedPlayer.GetComponent<Slider>().value, controlCurseur.CursorPowerFirePlayer.GetComponent<Slider>().value, controlCurseur.CursorDelayFirePlayer.GetComponent<Slider>().value);
             }
+
+            if(!isPlayer2){
+                    playerNames[0] = namePlayer.text;
+                    namePlayer.text = "";
+                    playerNameText.text = "Player 2";
+                    PlayerNameTextPanel.text = "Player 2";
+                    isPlayer2 = true;
+                } else {
+                    
+                        playerNames[1] = namePlayer.text;
+                    namePlayer.text = "";
+                    
+                    
+                    menuController.loadAllScene("game1");
+
+                    
+
+                }
             
 
 
